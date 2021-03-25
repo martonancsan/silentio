d09966873845d9cf363b042facd215b65b78412b
martonancsan <marton.ancsan@gmail.com>
2021-03-25 12:33:26 +0100

    Directory structure cleaned up further. gitignore files added, node installed in all modules (ie. model, controller)

--- scripts/http.sh
+++ scripts/http.sh
@@ -1,8 +1,8 @@
 # curl -d '{ "unitId": 749112039873, "measurement": { "5": 74, "10": 83, "15": 61, "20": 77, "25": 88, "30": 64, "35": 82, "40": 80, "45": 84, "50": 83, "55": 69, "60": 88 } }' -H "Content-Type: application/json" -X POST http://localhost:3000/data
 
 for i in {1..10}; do
-    echo -n "Data frame $i sent"
-    #curl -d "@data.json" -H "Content-Type: application/json" -X POST http://localhost:3000/data
+    echo -n "CLIENT: Data frame $i sent"
     date
-    sleep 1
+    curl -d "@data.json" -H "Content-Type: application/json" -X POST http://localhost:3000/data
+    sleep 2
 done
