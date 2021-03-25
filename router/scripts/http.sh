ea19192942cb042370a32487a0c6b87663ca3620
martonancsan <marton.ancsan@gmail.com>
2021-03-25 11:58:50 +0100

    Duplicate script library deleted from router

--- router/scripts/http.sh
+++
@@ -1,8 +0,0 @@
-# curl -d '{ "unitId": 749112039873, "measurement": { "5": 74, "10": 83, "15": 61, "20": 77, "25": 88, "30": 64, "35": 82, "40": 80, "45": 84, "50": 83, "55": 69, "60": 88 } }' -H "Content-Type: application/json" -X POST http://localhost:3000/data
-
-for i in {1..10}; do
-    echo -n "Data frame $i sent"
-    #curl -d "@data.json" -H "Content-Type: application/json" -X POST http://localhost:3000/data
-    date
-    sleep 1
-done
