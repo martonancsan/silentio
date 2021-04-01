350d4f164ea16d6342f50f1f38ee31581369d910
martonancsan <marton.ancsan@gmail.com>
2021-04-02 00:38:54 +0200

    space added

--- scripts/http.sh
+++ scripts/http.sh
@@ -1,7 +1,7 @@
 # curl -d '{ "unitId": 749112039873, "measurement": { "5": 74, "10": 83, "15": 61, "20": 77, "25": 88, "30": 64, "35": 82, "40": 80, "45": 84, "50": 83, "55": 69, "60": 88 } }' -H "Content-Type: application/json" -X POST http://localhost:3000/data
 
 for i in {1..10}; do
-    echo -n "CLIENT: Data frame $i sent at "
+    echo -n "CLIENT : Data frame $i sent at "
     date
     curl -d "@data.json" -H "Content-Type: application/json" -X POST http://localhost:3000/data
     sleep 2
